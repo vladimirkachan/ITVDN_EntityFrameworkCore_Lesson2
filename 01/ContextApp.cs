ce79f1f2164151159c1569201d0b111a228988c6
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-05 09:44:04 +0300

    Добавьте файлы проекта.

---
+++ 01/ContextApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace _01
+{
+    public class ContextApp : DbContext
+    {
+        public DbSet<Phone> Phones {get; set;}
+        public ContextApp()
+        {
+            Database.EnsureDeleted();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\;Database=TestDB_001;Trusted_Connection=True;");
+        }
+    }
+}
